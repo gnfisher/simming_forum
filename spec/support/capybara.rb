400bbdd9794240073bd069f4e080538dd3702b2d
Greg Fisher <gnfisher@thoughtbot.com>
2021-09-10 20:29:46 -0400

    Add rspec

---
+++ spec/support/capybara.rb
@@ -0,0 +1,15 @@
+require "capybara/rails"
+require "capybara/rspec"
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
+end
+
+Capybara.javascript_driver = :selenium_chrome_headless
+Capybara.disable_animation = true
